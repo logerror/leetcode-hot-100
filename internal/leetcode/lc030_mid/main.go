752759e7666698fc47080682bb765e49a6bbff48
fandong@coding.net <Fan123456@>
2023-11-15 23:25:49 +0800

    HOT + 1

--- internal/leetcode/lc030_mid/main.go
+++ internal/leetcode/lc030_mid/main.go
@@ -4,7 +4,8 @@ package lc030_mid
 基于快速排序的选择算法
 */
 func findKthLargest(nums []int, k int) int {
-
+	n := len(nums)
+	return quickSelect(nums, 0, n-1, n-k)
 }
 
 func quickSelect(nums []int, l, r, k int) int {
@@ -29,5 +30,87 @@ func quickSelect(nums []int, l, r, k int) int {
 		return quickSelect(nums, l, j, k)
 	} else {
 		return quickSelect(nums, j+1, r, k)
+	}
+}
+
+/*
+快排可以解决问题，但是它需要确定数组中所有元素的正确位置，对于本题而言，我们只需要确定第k大元素的位置pos,
+我们只需要确保pos左边的元素都比它小，pos右边的元素都比它大即可，不需要关心其左边和右边的集合是否有序，
+所以，我们需要对快排进行改进(以下称分区)，将目标值的位置pos与分区函数Partition求得的位置index进行比对，
+如果两值相等，说明index对应的元素即为所求值，如果index<pos，则递归的在[index+1, right]范围求解；
+否则则在[left, index-1]范围求解，如此便可大幅缩小求解范围
+*/
+
+func findKthLargest3(nums []int, k int) int {
+	TopKSplit(nums, len(nums)-k, 0, len(nums)-1)
+	return nums[len(nums)-k]
+}
+
+func Parition(nums []int, start, stop int) int {
+	if start >= stop {
+		return -1
+	}
+	pivot := nums[start]
+	l, r := start, stop
+	for l < r {
+		for l < r && nums[r] >= pivot {
+			r--
+		}
+		nums[l] = nums[r]
+		for l < r && nums[l] < pivot {
+			l++
+		}
+		nums[r] = nums[l]
+	}
+	// 循环结束，l与r相等
+	// 确定基准元素pivot在数组中的位置
+	nums[l] = pivot
+	return l
+}
+
+func TopKSplit(nums []int, k, start, stop int) {
+	if start < stop {
+		index := Parition(nums, start, stop)
+		if index == k {
+			return
+		} else if index < k {
+			TopKSplit(nums, k, index+1, stop)
+		} else {
+			TopKSplit(nums, k, start, index-1)
+		}
+	}
+}
+
+/*
+基于堆排序的选择方法
+*/
+func findKthLargest2(nums []int, k int) int {
+	heapSize := len(nums)
+	buildMaxHeap(nums, heapSize)
+	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		heapSize--
+		maxHeapify(nums, 0, heapSize)
+	}
+	return nums[0]
+}
+
+func buildMaxHeap(a []int, heapSize int) {
+	for i := heapSize / 2; i >= 0; i-- {
+		maxHeapify(a, i, heapSize)
+	}
+}
+
+func maxHeapify(a []int, i, heapSize int) {
+	l, r, largest := i*2+1, i*2+2, i
+	if l < heapSize && a[l] > a[largest] {
+		largest = l
+	}
+	if r < heapSize && a[r] > a[largest] {
+		largest = r
+	}
+	if largest != i {
+		a[i], a[largest] = a[largest], a[i]
+		maxHeapify(a, largest, heapSize)
 	}
 }
